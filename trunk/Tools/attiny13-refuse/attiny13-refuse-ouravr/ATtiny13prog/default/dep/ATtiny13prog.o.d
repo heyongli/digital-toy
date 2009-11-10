565f8ba137478bb5051b23c6fbe06971dabdce6f
heyongli <heyongli@bd95d526-8348-11de-9000-790dadb5e1c7>
2009-11-10 15:07:59 +0000

    宏定义整理: 分类 1) 硬件资源配置    PIN   完成    TIMER  todo    INTERRUPT  todo 2) 程序控制定义

--- trunk/Tools/attiny13-refuse/attiny13-refuse-ouravr/ATtiny13prog/default/dep/ATtiny13prog.o.d
+++ trunk/Tools/attiny13-refuse/attiny13-refuse-ouravr/ATtiny13prog/default/dep/ATtiny13prog.o.d
@@ -1,33 +1,32 @@
 ATtiny13prog.o: ../ATtiny13prog.c ../ATtiny13prog.h \
-  C:/WinAVR/bin/../lib/gcc/avr/3.4.3/../../../../avr/include/avr/io.h \
-  C:/WinAVR/bin/../lib/gcc/avr/3.4.3/../../../../avr/include/avr/sfr_defs.h \
-  C:/WinAVR/bin/../lib/gcc/avr/3.4.3/../../../../avr/include/inttypes.h \
-  C:/WinAVR/bin/../lib/gcc/avr/3.4.3/../../../../avr/include/stdint.h \
-  C:/WinAVR/bin/../lib/gcc/avr/3.4.3/../../../../avr/include/avr/iotn26.h \
-  C:/WinAVR/bin/../lib/gcc/avr/3.4.3/../../../../avr/include/avr/portpins.h \
-  C:/WinAVR/bin/../lib/gcc/avr/3.4.3/../../../../avr/include/avr/wdt.h \
-  C:/WinAVR/bin/../lib/gcc/avr/3.4.3/../../../../avr/include/avr/signal.h \
-  C:/WinAVR/bin/../lib/gcc/avr/3.4.3/../../../../avr/include/avr/interrupt.h \
-  C:/WinAVR/bin/../lib/gcc/avr/3.4.3/../../../../avr/include/avr/delay.h
+  C:/WinAVR/avr/include/avr/io.h C:/WinAVR/avr/include/avr/sfr_defs.h \
+  C:/WinAVR/avr/include/inttypes.h C:/WinAVR/avr/include/stdint.h \
+  C:/WinAVR/avr/include/avr/iotn2313.h \
+  C:/WinAVR/avr/include/avr/portpins.h \
+  C:/WinAVR/avr/include/avr/version.h C:/WinAVR/avr/include/avr/wdt.h \
+  C:/WinAVR/avr/include/avr/interrupt.h C:/WinAVR/avr/include/avr/delay.h \
+  C:/WinAVR/avr/include/util/delay.h
 
 ../ATtiny13prog.h:
 
-C:/WinAVR/bin/../lib/gcc/avr/3.4.3/../../../../avr/include/avr/io.h:
+C:/WinAVR/avr/include/avr/io.h:
 
-C:/WinAVR/bin/../lib/gcc/avr/3.4.3/../../../../avr/include/avr/sfr_defs.h:
+C:/WinAVR/avr/include/avr/sfr_defs.h:
 
-C:/WinAVR/bin/../lib/gcc/avr/3.4.3/../../../../avr/include/inttypes.h:
+C:/WinAVR/avr/include/inttypes.h:
 
-C:/WinAVR/bin/../lib/gcc/avr/3.4.3/../../../../avr/include/stdint.h:
+C:/WinAVR/avr/include/stdint.h:
 
-C:/WinAVR/bin/../lib/gcc/avr/3.4.3/../../../../avr/include/avr/iotn26.h:
+C:/WinAVR/avr/include/avr/iotn2313.h:
 
-C:/WinAVR/bin/../lib/gcc/avr/3.4.3/../../../../avr/include/avr/portpins.h:
+C:/WinAVR/avr/include/avr/portpins.h:
 
-C:/WinAVR/bin/../lib/gcc/avr/3.4.3/../../../../avr/include/avr/wdt.h:
+C:/WinAVR/avr/include/avr/version.h:
 
-C:/WinAVR/bin/../lib/gcc/avr/3.4.3/../../../../avr/include/avr/signal.h:
+C:/WinAVR/avr/include/avr/wdt.h:
 
-C:/WinAVR/bin/../lib/gcc/avr/3.4.3/../../../../avr/include/avr/interrupt.h:
+C:/WinAVR/avr/include/avr/interrupt.h:
 
-C:/WinAVR/bin/../lib/gcc/avr/3.4.3/../../../../avr/include/avr/delay.h:
+C:/WinAVR/avr/include/avr/delay.h:
+
+C:/WinAVR/avr/include/util/delay.h:
