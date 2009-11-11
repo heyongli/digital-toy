ed3fe9bad56ddc2d3f68b9295d80bf06b9fbff7f
heyongli <heyongli@bd95d526-8348-11de-9000-790dadb5e1c7>
2009-11-11 14:20:57 +0000

    整理 attin13 fuse recover的 timer设置部分

--- Tools/attiny13-refuse/attiny13-refuse-ouravr/ATtiny13prog/default/dep/ATtiny13prog.o.d
+++ Tools/attiny13-refuse/attiny13-refuse-ouravr/ATtiny13prog/default/dep/ATtiny13prog.o.d
@@ -1,8 +1,7 @@
 ATtiny13prog.o: ../ATtiny13prog.c ../ATtiny13prog.h \
   C:/WinAVR/avr/include/avr/io.h C:/WinAVR/avr/include/avr/sfr_defs.h \
   C:/WinAVR/avr/include/inttypes.h C:/WinAVR/avr/include/stdint.h \
-  C:/WinAVR/avr/include/avr/iotn2313.h \
-  C:/WinAVR/avr/include/avr/portpins.h \
+  C:/WinAVR/avr/include/avr/iotn26.h C:/WinAVR/avr/include/avr/portpins.h \
   C:/WinAVR/avr/include/avr/version.h C:/WinAVR/avr/include/avr/wdt.h \
   C:/WinAVR/avr/include/avr/interrupt.h C:/WinAVR/avr/include/avr/delay.h \
   C:/WinAVR/avr/include/util/delay.h
@@ -17,7 +16,7 @@ C:/WinAVR/avr/include/inttypes.h:
 
 C:/WinAVR/avr/include/stdint.h:
 
-C:/WinAVR/avr/include/avr/iotn2313.h:
+C:/WinAVR/avr/include/avr/iotn26.h:
 
 C:/WinAVR/avr/include/avr/portpins.h:
 
