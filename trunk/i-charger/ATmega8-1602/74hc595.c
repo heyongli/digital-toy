bae120b406d42374ddbf74c257205a18d5a29f05
heyongli <heyongli@bd95d526-8348-11de-9000-790dadb5e1c7>
2009-12-16 13:41:45 +0000

    add driver for 74HC595

---
+++ trunk/i-charger/ATmega8-1602/74hc595.c
@@ -0,0 +1,21 @@
+#include <avr/io.h>
+#include <util/delay.h>
+#include "bitops.h"
+
+
+#define  PORT_74HC   PORTD 
+#define  CLK    5    /* shift clock: 74HC595 pin 11*/
+#define  LATCH  6		 /*latch clock: 74HC595 pin 12*/
+#define  SDI          7		 /*serial data in: 74HC595 pin14*/
+
+void init_74hc(void)
+{
+    _pin_out(PORT_74HC, CLK);
+    _pin_out(PORT_74HC, LATCH);
+    _pin_out(PORT_74HC, SDI);
+
+    
+	PORTC = 0xff;
+	
+	PORTDefine();
+}
