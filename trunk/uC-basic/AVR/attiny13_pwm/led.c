b32ef780ad09ede6f5c15d0987f72b14dc56a635
heyongli <heyongli@bd95d526-8348-11de-9000-790dadb5e1c7>
2010-02-06 12:06:49 +0000

    attiny13 pwm 学习

---
+++ trunk/uC-basic/AVR/attiny13_pwm/led.c
@@ -0,0 +1,47 @@
+
+#include "bitops.h"
+#include <avr/io.h>
+#include <util/delay.h>
+
+#define LED_On(i)  _clear_bit(PORTB,i)	/* 输出低电平，使LED发光 */
+#define LED_Off(i) _set_bit(PORTB,i)	/* 输出低电平，使LED发光 */
+
+
+
+
+void led_init()
+{
+	/* 往前步进 */
+    char i;
+		for (i = 0; i < 3; i++){
+
+			PORTB = 0;
+			_delay_ms(200);
+ 		    LED_Off(1);
+ 		    LED_Off(2);
+			_delay_ms(200);
+		}
+
+}	
+
+
+char mod=2;
+
+
+void sharp_flash()
+{
+    char x;
+
+    for(x=0;x<7;x++){
+       //LED_On(mod&1?0:7);
+	   LED_On(mod&2?1:7);
+	   LED_On(mod&4?2:7);
+
+       _delay_ms(10);
+	   //LED_Off(0);
+	   LED_Off(1);
+ 	   LED_Off(2);
+	   	_delay_ms(10);
+	}
+   
+}
--- trunk/uC-basic/AVR/attiny13_pwm/pwm.c
+++ trunk/uC-basic/AVR/attiny13_pwm/pwm.c
@@ -44,27 +44,27 @@ COM01 COM00:
 TCCR0B :
 WGM20  
 
-CS02 CS01 CS00 瞿繙
+CS02 CS01 CS00 ㄩ
 0     1      0       clkI/O/8 (From prescaler)
 
 
 ------------------
 TCNT0  -
 
-OCR0A 穡C Output Compare Register , cmpare to TCN0
+OCR0A 每 Output Compare Register , cmpare to TCN0
 
-GTCCR 穡C General Timer/Counter Control Register
+GTCCR 每 General Timer/Counter Control Register
              Bit              7            0
                             TSM          PSR10     
              Read/Write     R/W           R/W
              Initial Value    0             0
-            Bit 7 穡C TSM: Timer/Counter Synchronization Mode
+            Bit 7 每 TSM: Timer/Counter Synchronization Mode
             Writing the TSM bit to one activates the Timer/Counter Synchronization mode. In this mode, the
             value that is written to the PSR10 bit is kept, hence keeping the Prescaler Reset signal asserted.
             This ensures that the Timer/Counter is halted and can be configured without the risk of advanc-
             ing during configuration. When the TSM bit is written to zero, the PSR10 bit is cleared by
             hardware, and the Timer/Counter start counting.
-            Bit 0 穡C PSR10: Prescaler Reset Timer/Counter0
+            Bit 0 每 PSR10: Prescaler Reset Timer/Counter0
             When this bit is one, the Timer/Counter0 prescaler will be Reset. This bit is normally cleared
             immediately by hardware, except ifexcept )
 
