4114004639d614a54b38e3d721ef9d39ec3cf094
heyongli <heyongli@bd95d526-8348-11de-9000-790dadb5e1c7>
2010-05-16 13:23:21 +0000

    Braintenberg Robot code

---
+++ trunk/e-kits/tiny-uC-v0/tiny-uC-Braitenberg/braiten-berg.h
@@ -0,0 +1,12 @@
+#ifndef _BRAITN_BERG_
+#define _BRAITN_BERG_
+
+
+#define L_MOTOR  OCR0A   /* PWM PIN */
+#define LEYE_ADC    3  /*adc chanel*/
+
+#define R_MOTOR  OCR0B                         
+#define REYE_ADC    1 
+
+
+#endif
