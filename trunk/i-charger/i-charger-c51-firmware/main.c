119c0a91e09d7070b87acb224947f7502b32d659
heyongli <heyongli@bd95d526-8348-11de-9000-790dadb5e1c7>
2010-06-07 15:16:48 +0000

    1. adc filter 2. basic charger

--- trunk/i-charger/i-charger-c51-firmware/main.c
+++ trunk/i-charger/i-charger-c51-firmware/main.c
@@ -14,6 +14,16 @@ extern float  refV ;
 char t=0;
 unsigned short i=0;
 
+//easy charging
+unsigned char duty=0, max_duty=15;
+float current=0, voltage=0;
+float vlimit = 7.0 ; //V
+float climit = 1.1 ; //A
+float ir = 0 ;  //battery internal resistor
+void easy_charging();
+
+char charging = 0;  //not incharging
+void isdone();
 
 //i_charger charger = INIT_CHARGER;
 
@@ -23,7 +33,7 @@ void io_init()
    pwm_init(); //pull down pwm
    lcd1602_init();
     lcd_cursor(0,0);
-   lcd_puts("Welcom!! Dtoy");
+   lcd_puts("Welcom!");
    seg_init();
    mdelay(100);
  
@@ -32,7 +42,7 @@ void main()
 {
    unsigned short n,x=0;
    unsigned short onduty=0;
-   unsigned short adc;
+   float adc;
 
 
    io_init();
@@ -48,28 +58,192 @@ void main()
 // KEY_FUNC P3_3  KEY_DISCHARGER  P3_2  KEY_RESUME    P3_1  
     
      if(key(KEY_OK))
-	    pwm_setduty(i++);
+	    ;
 
-   	 if(key(KEY_DIS))
-	    bl_on();
+   	 if(key(KEY_DIS)) i++;
+	   
     
-	 if(key(KEY_RES))
-	    bl_off();
+	 if(key(KEY_RES)) i--;
 
-	  adc=adc0832(0);
+   
+	  voltage=adc_V();
       lcd_cursor(0,0);
-      lcd_puts("Ch 0: ");
-	  print10(adc);
+      lcd_puts("V: ");
+	  showVA(voltage*100);
 
-	  adc=adc0832(1);
+	  current=adc_A();
   	  lcd_cursor(0,1);
-      lcd_puts("Ch 1: ");
-  	  print10(adc);
+      lcd_puts("A: ");
+  	  showVA(current*100);
 	  
-	  lcd_puts("  ");
-  	  print10(x++);
+	  lcd_puts(" pwm:");
+  	  print10(duty);
+
+	/*charging core*/
+	 isdone();
+	  easy_charging();
+  	 
+   /*end charging*/	
 	  
    }
   
   
 }
+
+adj_c()
+{
+
+  char  i=0;
+
+   for( i=1;i<max_duty;i++){
+     current=adc_A();    
+     if(current < climit){
+        duty+=1;
+	   if(duty>max_duty)
+	       duty=max_duty;
+
+     }
+     
+     if(current > climit){
+        if(duty>0)
+	         duty-=1;
+	    if(duty==0)
+	       duty==1;
+    }
+
+	 pwm_setduty(duty);
+	 current=adc_A();
+	 voltage=adc_V();
+		  voltage=adc_V();
+      lcd_cursor(0,0);
+      lcd_puts("V: ");
+	  showVA(voltage*100);
+
+	  current=adc_A();
+  	  lcd_cursor(0,1);
+      lcd_puts("A: ");
+  	  showVA(current*100);
+   }  
+
+}
+void isdone()
+{
+    float bv=0;
+  
+   if(voltage < 0.5 || voltage>8.0) /*no battery or pull out*/
+      charging = 0;
+
+    if(1==charging) return; /*in charging*/
+
+    /*try new charging */
+    /*para*/
+	vlimit = 7.0; /*test battry*/
+
+    /*test battery*/
+     pwm_setduty(0);
+ 	 mdelay(100);
+     bv=voltage=adc_V();    	
+     if(voltage> 3)
+	    charging = 1;
+
+	 if(0==charging) /*no battery*/
+	    return;
+
+	/*test internal resistor*/
+    pwm_setduty(5);
+	mdelay(100);
+	mdelay(100);
+  	  current=adc_A();
+	  voltage=adc_V();
+		  voltage=adc_V();
+      lcd_cursor(0,0);
+      lcd_puts("V: ");
+	  showVA(voltage*100);
+
+	  current=adc_A();
+  	  lcd_cursor(0,1);
+      lcd_puts("A: ");
+  	  showVA(current*100);
+
+	pwm_setduty(0);
+	ir = (voltage-bv)/current;
+    
+	lcd_cursor(9,0);
+    lcd_puts("ir:");
+	print10(1000*ir);
+
+	if(ir>1.5) {
+	   lcd_cursor(9,0) ;
+	   mdelay(100); mdelay(100);	mdelay(100);
+       mdelay(100);	mdelay(100);
+       lcd_puts("bad ir  ");
+	}
+	   
+	/*set parameter */
+	 vlimit = 6.9;
+	 climit = 1.2;
+
+     /*init start*/
+     pwm_setduty(1);
+	 mdelay(100);mdelay(100);
+	 mdelay(100);mdelay(100);
+	 current=adc_A();
+	 voltage=adc_V();
+	 duty=1;
+	    /*adjust the current*/
+
+	 adj_c();
+
+}
+
+void easy_charging()
+{
+   if(!charging) return;
+
+   if(duty==0) goto done; /*already complete charging*/
+  
+
+
+   
+
+   /*ending test*/
+   if(current <0.100) /*drop to 50mA, so done*/
+       goto done;
+
+   if( voltage > (vlimit+ir*current)){
+   	   if(duty>0)
+	        duty-=1;
+	   if(duty==0)
+	      goto done;
+   
+   }
+    
+   pwm_setduty(duty);	  
+        
+      
+
+   return;
+/* complete the charging */
+   done:
+   {
+      duty=0;
+      pwm_setduty(duty);
+
+	  bl_off();
+      mdelay(55);
+	  bl_on();
+   	  mdelay(55);
+	  bl_off();
+      mdelay(55);
+	  bl_on();
+   	  mdelay(55);
+	  bl_off();
+      mdelay(55);
+	  bl_on();
+   	  mdelay(55);
+	  
+   }
+
+
+}
+
