fc3164cac8515f890c0457eb109188f88f196639
heyongli <heyongli@bd95d526-8348-11de-9000-790dadb5e1c7>
2010-09-01 13:15:56 +0000

    mega88 time0: HZ system ok

--- ideas-now/i-uC/Mega88-i-uC-statrup/timer0.c
+++ ideas-now/i-uC/Mega88-i-uC-statrup/timer0.c
@@ -1,10 +1,10 @@
 #include "include/avrio.h"
 #include <avr/interrupt.h>
 
-/* timer0,  fcpu=4Mhz, 
- * prescaer:clkio/64, 4M/64= 62500HZ, 
- * 1 ticks= 1000/62500=0.016ms, 
- * 1ms ticks:1/0.016=62.5
+/* timer0,  fcpu=10Mhz 
+ * prescaer:clkio/64, 10M/64= 156250HZ, 
+ * 1 ticks= 1000/156250=0.0064ms, 
+ * 1ms ticks:1/0.0064=156.25
  */
 #define TIMER0_HZ  ((F_CPU)/(64UL))
 #define TIMER0_TCN (255UL- ((TIMER0_HZ)/(HZ)) )  /*62.5 times OVERFLOW, 0.5 for ISR overhead...*/
@@ -26,7 +26,7 @@ ISR(TIMER0_OVF_vect)
    jiffers++;
    static unsigned long mark= 0;
    if(timeafter(jiffers,mark+ HZ/8 )){ //4times per second
-      _toggle_bits(LED_PORT,0b11); //hart beats
+      _toggle_bits(LED_PORT,0b100); //hart beats
 	  mark = jiffers;
    }
 
@@ -36,26 +36,60 @@ ISR(TIMER0_OVF_vect)
 
 
 
+/*
+from mega88:
+----mode CTC---
+CTC timer  TOP:OCR0A  update OCRx at: Immediate  TOV flag set on:MAX
+CTC opration: TCNT0 count up to OCR0A then been cleared
+   
+*/
 void timer0_init()
 {
 
-  TCCR0  =   (_bits8(0b11,CS00,CS02)) ;
+#if defined (__AVR_ATmega8__)
+  //mega8 timer0 only used for count: always up to MAX (0xFF) and start again from 0
+  TCCR0  =   (_bits8(0b11,CS00,CS02)) ; //clk(io)/64
   TCNT0  = TIMER0_TCN; //1ms per interrupt
  
-  //TIMSK |= _bits8(1,TOIE0,TOIE0); //enable interrupt;
+  TIMSK |= _bits8(1,TOIE0,TOIE0); //enable overflow interrupt;
   
   //TIFR TOV0, clear by interrupt
- // SFIOR |=  PSR10;
+   SFIOR |=  PSR10;
+#elif  defined (__AVR_ATmega88__)
+   //mega88 timer0  normal mode:  always up to MAX (0xFF) and start again from 0
+   TCCR0A  =   0;  
+   TCCR0B  =  (_bits8(0b11,CS00,CS02)) ;//clk(io)/64
+   TCNT0  = TIMER0_TCN; //1ms per interrupt
+ 
+   TIMSK0 |= _bits8(1,TOIE0,TOIE0); //enable over flow interrupt;
+  
+  //prescaler reset
+   GTCCR  |=  PSRSYNC;
+#else
+   #error "cpu not support !"
+#endif
 
 }
 
 
 void enable_timer0()
 {
+#if defined (__AVR_ATmega88__)
+   TIMSK0 |= _bits8(1,TOIE0,TOIE0);;// enable interrupt;
+#elif  defined (__AVR_ATmega8__)
    TIMSK |= _bits8(1,TOIE0,TOIE0);;// enable interrupt;
+#else
+   #error "cpu not support !"
+#endif 
 
 }
 void disable_timer0()
 {
+#if defined (__AVR_ATmega88__)
+    TIMSK0  &= ~_bits8(1,TOIE0,TOIE0);// enable interrupt;
+#elif defined (__AVR_ATmega8__)
     TIMSK  &= ~_bits8(1,TOIE0,TOIE0);// enable interrupt;
+#else
+   #error "cpu not support !"
+#endif
 }
