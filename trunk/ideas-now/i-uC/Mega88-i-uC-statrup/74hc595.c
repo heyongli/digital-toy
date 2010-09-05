9774d463e121dba400b16a257f560d62d6918024
heyongli <heyongli@bd95d526-8348-11de-9000-790dadb5e1c7>
2010-09-05 11:35:52 +0000

    3wire-1602 all done: lcd stack on the Hrdunio or via external Dupont Line ; key up/down ok

--- trunk/ideas-now/i-uC/Mega88-i-uC-statrup/74hc595.c
+++ trunk/ideas-now/i-uC/Mega88-i-uC-statrup/74hc595.c
@@ -1,11 +1,22 @@
 #include <util/delay.h>
 #include "include/avrio.h"
 
+//#define EXT_CONNETOR
 
-#define  PORT_74HC595   PORTD 
+
+#ifdef EXT_CONNETOR
+#define  PORT_74HC595   PORTB 
 #define  CLK    5    /* raise-edge , shift clock: 74HC595 pin 11*/
-#define  LATCH  6	 /* raise-edge output to Qx, latch clock: 74HC595 pin 12*/
-#define  SDI    7	 /* CLK raise edge deposit, sserial data in: 74HC595 pin14*/
+#define  LATCH  4	 /* raise-edge output to Qx, latch clock: 74HC595 pin 12*/
+#define  SDI    3	 /* CLK raise edge deposit, sserial data in: 74HC595 pin14*/
+#define pin_init() _pins_mode(PORT_74HC595, SDI,CLK,OUTPUT)
+#else  //stack on Hrdunio
+#define  PORT_74HC595   PORTD 
+#define  CLK    3    /* raise-edge , shift clock: 74HC595 pin 11*/
+#define  LATCH  4	 /* raise-edge output to Qx, latch clock: 74HC595 pin 12*/
+#define  SDI    2	 /* CLK raise edge deposit, sserial data in: 74HC595 pin14*/
+#define pin_init() _pins_mode(PORT_74HC595, SDI,LATCH,OUTPUT)
+#endif
 
 
 static void delay_io(void)
@@ -16,7 +27,7 @@ static void delay_io(void)
 
 void init_74hc595(void)
 {
-    _pins_mode(PORT_74HC595, CLK,SDI,OUTPUT);
+    pin_init();
 }
 
 void shiftout(unsigned char data)
