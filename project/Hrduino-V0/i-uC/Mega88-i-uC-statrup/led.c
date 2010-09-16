ce8725d7759fa20561c8c3703c041f2dec278b71
heyongli <heyongli@bd95d526-8348-11de-9000-790dadb5e1c7>
2010-09-16 09:32:15 +0000

    Hrduino V0 include 3 componet in it's Core: 1. the basic system : i-uC 2. the 3-wired LCD debuging shield 4. the io-extention board

---
+++ project/Hrduino-V0/i-uC/Mega88-i-uC-statrup/led.c
@@ -0,0 +1,39 @@
+#include "include/avrio.h"
+#include <util/delay.h>
+
+#define LED_PORT PORTD
+
+#define LED_On(i)  _clear_bit(LED_PORT,i)	/* 输出低电平，使LED发光 */
+#define LED_Off(i) _set_bit(LED_PORT,i)	/* 输出低电平，使LED发光 */
+
+
+
+
+void led_init()
+{
+	_pins_mode(LED_PORT,0,1,OUTPUT);//_nm8(0b11,0,1);
+}	
+
+
+
+
+#if 0
+char mod=2;
+void sharp_flash()
+{
+    char x;
+
+    for(x=0;x<19;x++){
+   	   LED_On(mod&2?0:7);
+	   LED_On(mod&4?1:7);
+
+       _delay_ms(20);
+	 
+
+	   LED_Off(0);
+ 	   LED_Off(1);
+	   	_delay_ms(20);
+	}
+   mod++;
+}
+#endif
