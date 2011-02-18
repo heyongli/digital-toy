4fe93ebfd2b5b0451416fb4c0b3fbb85ae189e9a
heyongli <heyongli@bd95d526-8348-11de-9000-790dadb5e1c7>
2011-02-18 09:49:01 +0000

    DA1302 debuging

--- ideas-now/51-Zduino/51z-firmware/driver/4wire-1602.c
+++ ideas-now/51-Zduino/51z-firmware/driver/4wire-1602.c
@@ -149,6 +149,7 @@ void lcd_puts(unsigned char *s)
      
 }
 
+#define lcd_putc(c) send_data(c)
 
 char hex2c(char hex)
 {
@@ -158,26 +159,46 @@ char hex2c(char hex)
    else
       return 'a'+hex-0xa;
 }
+
+
+lcd_showhex(unsigned char x)
+{
+  
+    lcd_putc(hex2c(((x&0xF0)>>4)));
+    lcd_putc(hex2c(((x&0xF)>>4)));
+
+}
+
 void print10(unsigned short n)
 {
-     static char four[6];
+     static char four[6], c;
      //irqoff();
-	 four[0]= hex2c( (n/10000) );
+	 c=four[0]= hex2c( (n/10000) );
 	 n = n%10000;
-	 four[1]= hex2c(n/1000);
-	 n = n%1000;
-		   	
-	 four[2]= hex2c( (n/100) );
-	 n = n%100;
-	
-	 four[3]= hex2c( (n/10) );
-	 four[4]= hex2c( (n%10) );;
-	 four[5]= 0;
+     
+	 if(c) lcd_putc(c);	 
 
-     if(four[0] == '0')
-	    lcd_puts(four+1);
-	 else
-	 	lcd_puts(four);
+	 c=four[1]= hex2c(n/1000);
+	 n = n%1000;
+	 //if(c) lcd_putc(c);	 
+		   	
+	 c=four[2]= hex2c( (n/100) );
+	 n = n%100;
+	 //if(c) lcd_putc(c);	 
+	
+	 c=four[3]= hex2c( (n/10) );
+     lcd_putc(c);	 
+
+	 c=four[4]= hex2c( (n%10) );;
+	 lcd_putc(c);	 
+
+
+	 c=four[5]= 0;
+
+     //if(four[0] == '0')
+	   // lcd_puts(four+1);
+	 //else
+	 //	lcd_puts(four);
 }
 
 void showVA(unsigned short n)
