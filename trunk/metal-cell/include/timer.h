843eae91992f2522d8e984da6af7493a0601739c
heyongli <heyongli@bd95d526-8348-11de-9000-790dadb5e1c7>
2011-04-20 06:42:39 +0000

    rebuild c51 with metal cell

--- trunk/metal-cell/include/timer.h
+++ trunk/metal-cell/include/timer.h
@@ -20,8 +20,25 @@
 /*  
  * tick/jiffers system 
  */
-#define HZ  1000  
 extern volatile unsigned  long jiffers;
+
+#ifdef ARCH_AVR
+#define HZ  1000  
+#endif
+
+#ifdef ARCH_C51
+/* 20Mhz  1ms = 1000us
+ *    5ms jiffers 
+ *    10kHz PWM (100us per pusle,25step, 40us mini-cycle,PWMCYCLE=400us/0.6 )
+ *    0-125us on-duty      
+ */
+#define MS_CYCLE  8330 /*5ms*/
+#define PWM_CYCLE 60   /*must >50, < 50 will consume all cpu*/	  /*100+[~140] cycles*/ //give up 1000+ left_cpu_of_timer
+#define PWM_STEP  20  /*one cell get 10 mA at duty<4*/
+#define HZ  (((unsigned long)1000*1000)/(unsigned long)MS_CYCLE) /*jiffers per MS*/
+             //(1000*1000)/MS_CYCLE, 不行... 数据类型可能是char...
+#endif
+
 
 
 #define timeafter(a,b)         \
