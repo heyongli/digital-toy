0d90647b31c9dc727cd68cc82cffeb6507a3d3f8
heyongli <heyongli@bd95d526-8348-11de-9000-790dadb5e1c7>
2011-05-01 11:04:30 +0000

    remove tmp filem,and check program size..

--- trunk/ideas-now/c51-tiny13-meter/i2c-slave-metal/main.c
+++ trunk/ideas-now/c51-tiny13-meter/i2c-slave-metal/main.c
@@ -6,42 +6,34 @@
 #include <drivers/4wire-1602.h>
 
 
-char key(char key)
-{
-   _test_key(key);
-}
-
-char wait_key(char key)
-{
-   _wait_key(key);
-
-}
-
-
 int main()
 {
 
    
 	cli();
     //system timer init
-	timer0_init();
-	enable_timer0();
+//	timer0_init();
+//	enable_timer0();
     sti();
 
      //key LCD shiled
-	lcd1602_init();
+	//lcd1602_init();
 
-
+ 	adc_init();
   
-	_key_init();
+	//_key_init();
 
     
     
 	//_pins_mode(LED_PORT,0,2,OUTPUT);//_nm8(0b11,0,1);
 	while(1){
-	
+	  unsigned short x=_adc(1);
 
 	  i2c_demo();
+
+       x*=2;
+
+
 	  
 	
 	}
