2751e3d4f4b21c69c34295ec1d721fe1b9432044
heyongli <heyongli@bd95d526-8348-11de-9000-790dadb5e1c7>
2011-05-14 13:17:09 +0000

    add print10L(n,base), base specify how many bits to print: 10, 2 bit 100, 3bit....

--- metal-cell/include/uprint.h
+++ metal-cell/include/uprint.h
@@ -5,6 +5,7 @@ void lcd_putc(char data);
 
 char hex2c(char hex);
 void print10(unsigned long n);
+void print10L(unsigned long n, unsigned long base);
 void printLL(unsigned long n, char dot, char prec);
 
 
