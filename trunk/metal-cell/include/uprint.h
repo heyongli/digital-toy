be65fbea713e04c00cb71166c53a813ee122badf
heyongli <heyongli@bd95d526-8348-11de-9000-790dadb5e1c7>
2011-06-27 13:30:21 +0000

    optimized the print10: with width, dot postion

--- trunk/metal-cell/include/uprint.h
+++ trunk/metal-cell/include/uprint.h
@@ -5,7 +5,12 @@ void lcd_putc(char c);
 void lcd_hex8(unsigned char x);
 char hex2c(char hex);
 void print10(unsigned long n);
-void print10L(unsigned long n, unsigned long base);
+
+/*
+ *  width: total number to show
+ *  dot: postion of the dot, 0 is no dot, 1 :xxx.x, 2:xx.xx
+ */
+void print10L(unsigned long n, unsigned long width,char dot);
 void printLL(unsigned long n, char dot, char prec);
 
 
