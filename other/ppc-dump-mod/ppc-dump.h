12373e10efcba54c989789fba584aa51447db08c
heyongli <heyongli@bd95d526-8348-11de-9000-790dadb5e1c7>
2011-12-07 08:57:53 +0000

    seprated module

---
+++ other/ppc-dump-mod/ppc-dump.h
@@ -0,0 +1,26 @@
+#ifndef PPC_DUMP_H
+#define PPC_DUMP_H
+
+/* mask ops */
+/*bits define */
+/* 31 30 .....      4  3  2  1  0*/
+//				e      s
+/* 0 1 2 3 4 ......................31*/  //BE
+//    s      e
+#define _MASK32(s,e)      ((0xFFFFffff<<(s))&(0xFFFFffff>>(31-(e))))
+                          /*s=2, e=3*/
+					          /* xxxx XX00  	& 	 0000 XXXX*/
+
+#define  _VAL32(val,s,e) (  ((val)&(_MASK32((s),(e))) )>>(s))
+#define  _VAL32BE(val,s,e)  (_VAL32(val,(31-e), (31-s)))
+
+//make it simple
+//#define  _VBE(val,s,e) ({ typeof(val) _xval=-1;  if(sizeof(val)==32)_xval=_VAL32BE(val,s,e);  _xval; })
+
+
+
+
+
+
+
+#endif
