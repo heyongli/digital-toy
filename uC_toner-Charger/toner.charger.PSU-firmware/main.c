735e87c838e4f58fffd57fe502ba2c7927f0c126
heyongli <heyongli@bd95d526-8348-11de-9000-790dadb5e1c7>
2012-07-04 14:28:22 +0000

    rename

---
+++ uC_toner-Charger/toner.charger.PSU-firmware/main.c
@@ -0,0 +1,36 @@
+#include "config.h"
+
+#include <avr/interrupt.h>
+#include <util/delay.h>
+
+#include <io.h>
+#include  <bitops.h>
+
+
+
+
+/* 
+  0   5 6 7  8...   15
+  PWM:000    Detect...
+  0000mA        0000mV
+       5        10
+*/
+
+int main()
+{
+	timer0_init();
+	enable_timer0();
+	lcd1602_init();
+	adc_init();
+
+	io_init();
+	
+	lcd_puts("Tool gear V0");
+    sti();
+
+	while(1){
+		update_lcd();
+		iron_loop();
+	}
+
+}
