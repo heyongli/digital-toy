bf6bb721d851a56e7f1f521093a282e449f859cf
heyongli <heyongli@bd95d526-8348-11de-9000-790dadb5e1c7>
2013-10-11 01:40:01 +0000

    new BandSpread Caculator

--- trunk/e-calculator/collection/Bandspread Calculator_files/BandspreadCalc.js
+++ trunk/e-calculator/collection/Bandspread Calculator_files/BandspreadCalc.js
@@ -1,4 +1,4 @@
-// Created by iWeb 2.0.4 local-build-20120315
+// Created by iWeb 2.0.4 local-build-20130503
 
 setTransparentGifURL('Media/transparent.gif');function applyEffects()
 {var registry=IWCreateEffectRegistry();registry.registerEffects({stroke_0:new IWPhotoFrame([IWCreateImage('BandspreadCalc_files/Formal_inset_01.png'),IWCreateImage('BandspreadCalc_files/Formal_inset_02.png'),IWCreateImage('BandspreadCalc_files/Formal_inset_03.png'),IWCreateImage('BandspreadCalc_files/Formal_inset_06.png'),IWCreateImage('BandspreadCalc_files/Formal_inset_09.png'),IWCreateImage('BandspreadCalc_files/Formal_inset_08.png'),IWCreateImage('BandspreadCalc_files/Formal_inset_07.png'),IWCreateImage('BandspreadCalc_files/Formal_inset_04.png')],null,0,0.800000,1.000000,1.000000,1.000000,1.000000,14.000000,14.000000,14.000000,14.000000,191.000000,262.000000,191.000000,262.000000,null,null,null,0.100000)});registry.applyEffects();}
@@ -6,6 +6,6 @@ function hostedOnDM()
 {return false;}
 function onPageLoad()
 {loadMozillaCSS('BandspreadCalc_files/BandspreadCalcMoz.css')
-adjustLineHeightIfTooBig('id1');adjustFontSizeIfTooBig('id1');detectBrowser();adjustLineHeightIfTooBig('id2');adjustFontSizeIfTooBig('id2');adjustLineHeightIfTooBig('id3');adjustFontSizeIfTooBig('id3');Widget.onload();fixAllIEPNGs('Media/transparent.gif');applyEffects()}
+adjustLineHeightIfTooBig('id1');adjustFontSizeIfTooBig('id1');adjustLineHeightIfTooBig('id2');adjustFontSizeIfTooBig('id2');adjustLineHeightIfTooBig('id3');adjustFontSizeIfTooBig('id3');detectBrowser();Widget.onload();fixAllIEPNGs('Media/transparent.gif');applyEffects()}
 function onPageUnload()
 {Widget.onunload();}
